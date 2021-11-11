15c57dde7518f6a5bf6ba4422ac95e3c6a2cbb1a
zuzu <dlwnghks6821@naver.com>
2021-11-11 21:37:12 +0900

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { InputGroup,FormControl,Nav,NavDropdown,Button} from 'react-bootstrap';
+import { InputGroup,FormControl,Nav,NavDropdown,Button,Table} from 'react-bootstrap';
 import { useState,useEffect } from 'react';
 import axios from 'axios'; 
 import api_key from './Apikey'; // api key 
@@ -10,7 +10,7 @@ import {Link , Route } from 'react-router-dom';
 
 function App() {
   
-  
+ 
   // 검색한 소환사의 이름을 저장하는 state 
   let [summonerName , setSummonerName] = useState(null);
  
@@ -23,19 +23,18 @@ function App() {
 
   //검색한 소환사의 매치 id list 를 가져옵니다.
   let[getMatchIdList , setMatchIdList] = useState(null);
- 
+  
+  // timestamp!
   let[revDate , setRevDate] = useState(null);
 
-  // 검색한 소환사의 전적에서 참가한 소환사들의 정보 ( kda , 챔피언 , 아이템정보 등을 저장합니다 .)
+  // 검색한 소환사의 매치 정보 데이터 
   let[getSummonInfo , SetSummonInfo] = useState(null);
   
   // 모달창의 상태 여부 state
   let [modalStatus,setModalStatus] = useState(false);
-  
-  
-
-  const PromiseArr = [];
-  
+  // 게임리스트 가져오기 버튼 상태여부 state 
+  let[fetchGameListButton, setFetchGameListButton] = useState(false);
+ 
  /**
   *  소환사의 이름을 input 에서 받아옵니다. 
   * @param {*} e 
@@ -53,6 +52,7 @@ function App() {
  * 사용자가 검색버튼을 누르면 해당 검색창의 parameter를 이용하여 데이터를 fetch 합니다. 그후 모달창을 띄웁니다.
  */
    async function ChangeStatus(e){
+   
     console.log("처음 1");
   
     if(!summonerName){
@@ -145,39 +145,106 @@ function App() {
          
             }
             
-            
-           // SetSummonInfo(users)
+          
 
             console.log("promise all 6");       
           // 데이터를 가져오고 모달창을 켭니다. 
           console.log("모달 상태 true 7");
-          if(modalStatus === false){
-            console.log("모달상태 true 로 변경 ")
-            setModalStatus(true);
-           
-          }
+         
+          
           console.log("전적 데이터 리스트 for loop axios 5");
           Promise.all(promises).then(()=>{ SetSummonInfo(users)})
-
-          
+          setModalStatus(true);
   }
+    /**
+     * @desc 
+     * 게임데이터 가져오기 버튼을 클릭하면 렌더링해주는 부분을 true 값으로 설정해줍니다. 
+     * render component : RenderingGameList
+     */
+    function FetchGameList(){
+      setFetchGameListButton(true);
+  
+    }
+    /**
+     * 
+     * @returns 
+     * @desc
+     * 가져와서 저장한 게임 전적 리스트를 렌더링해줍니다.
+     */
+
+      const RenderingGameList = () =>{
+        console.log('getSummonInfo => ' , getSummonInfo );
+      
+
+        getSummonInfo.map(function(items,index){
+          console.log("index start => " + index);
+
+          getSummonInfo[index].data.info.participants.map(function(items, index2){
+            //var party =  getSummonInfo[index].data.info.particip∂ants;
+              //console.log("index : " + index);
+            
+              console.log("championame => " + getSummonInfo[index].data.info.participants[index2].championName);
+          });
+
+        });
+        
+        return(
+          <div className="gameDetail"> 
+        <div className="container_001">
+        {
+          getSummonInfo.map(function(items){
+            return<div className="game_mode_Container"> 
+          <div className="matchDetail">
+            <div className="game_mode">
+              {items.data.info.gameMode}
+            </div>
+            </div>
+          </div>
+          })     
+        }
+      </div>
+          <div className="container_002">
+          {
+
+          getSummonInfo.map(function(items,index){
+            getSummonInfo[index].data.info.participants.map(function(items, index2){
+            console.log("championame => " + getSummonInfo[index].data.info.participants[index2].championName);
+
+            return<div className="championImageContainer">
+              
+            <div className="championImage">
+              <h1>hello</h1>
+            {getSummonInfo[index].data.info.participants[index2].championName}
+            </div>
+
+            </div>
+
+              });
+
+          })
+
+           
+            }
+              </div>
+
+              
+
+
+
+            </div>
+        
+
+        )
+      }
+
+  
 
   /**
    * 가져온 소환사의 정보를 rendering 해줍니다. ( Modal status === true 일 경우에 동작 )
    * @returns 
    */
   const GetRendering  = ()=> { 
-   
-   console.log(getSummonInfo);
-   console.log("데이터 렌더링 8 ");
-  
-  // getMatchIdList2();
-  
-   
-    
-        
-
-    
+ 
     return(
       <div className="renderingSummonerInfo">
         <h1>
@@ -194,27 +261,28 @@ function App() {
 
 
         <div>  </div>
-
-  
-       
         <div className="rankArea"><img src={'https://opgg-static.akamaized.net/images/medals/'+getUserRank.tier+'_1.png?image=q_auto:best&v=1'}/> 
         <span className="tier">{getUserRank.tier} {getUserRank.rank}</span>
         <div> {getUserRank.queueType}</div>
         {getUserRank.leaguePoints}LP / 승: {getUserRank.wins} 패 : {getUserRank.losses}</div>
-     
-   
-
+        
+        {
+        fetchGameListButton === true 
+        ? <RenderingGameList/> 
+        : null
+        }
+        <div className="fetchingButton" onClick={FetchGameList}>
+        <span>게임 데이터 가져오기 </span>
+        </div>
+        
+    
         <div>
          
        
 
 
         </div>
-        
-        
-
-        
-        
+       
       
       </div>
     )
@@ -248,7 +316,7 @@ function App() {
     : null
   }
    
-
+  
     </div>
   );
 }
